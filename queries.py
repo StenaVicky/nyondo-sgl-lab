0bec85192e72457a96c38501cd61d961059ef2b3
Omali stena vicky <omalstena55@gmail.com>
2026-04-21 22:21:29 +0300

    vulnerable work

--- queries.py
+++ queries.py
@@ -3,25 +3,25 @@ import sqlite3
 conn = sqlite3.connect('nyondo_stock.db')
 cursor = conn.cursor()
 
-# Query A
-print("A:", cursor.execute("SELECT * FROM products").fetchall())
+# # Query A
+# print("A:", cursor.execute("SELECT * FROM products").fetchall())
 
-# Query B
-print("B:", cursor.execute("SELECT name, price FROM products").fetchall())
+# # Query B
+# print("B:", cursor.execute("SELECT name, price FROM products").fetchall())
 
-# # Query C
-print("C:", cursor.execute("SELECT * FROM products WHERE id = 3").fetchall())
+# # # Query C
+# print("C:", cursor.execute("SELECT * FROM products WHERE id = 3").fetchall())
 
-# # Query D
-print("D:", cursor.execute("SELECT * FROM products WHERE name LIKE '%sheet%'").fetchall())
+# # # Query D
+# print("D:", cursor.execute("SELECT * FROM products WHERE name LIKE '%sheet%'").fetchall())
 
-# # Query E
-print("E:", cursor.execute("SELECT * FROM products ORDER BY price DESC").fetchall())
+# # # Query E
+# print("E:", cursor.execute("SELECT * FROM products ORDER BY price DESC").fetchall())
 
-# # Query F
-print("F:", cursor.execute("SELECT * FROM products ORDER BY price DESC LIMIT 2").fetchall())
+# # # Query F
+# print("F:", cursor.execute("SELECT * FROM products ORDER BY price DESC LIMIT 2").fetchall())
 
-# # Query G (Update)
+# # # Query G (Update)
 cursor.execute("UPDATE products SET price = 38000 WHERE id = 1")
 conn.commit()
 
